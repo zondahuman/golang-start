75aa0b4a57767c33a22ed2e907719530ee3cc236
zondahuman <zondahuman@gmail.com>
2017-05-18 23:59:45 +0800

    add

--- start/main.go
+++ start/main.go
@@ -9,7 +9,8 @@ func main() {
 	fmt.Println("df")
 	im.Han()
 	//var httpUrl = "http://localhost:7100/team/call1"
-	var httpUrl = "http://localhost:8300/team/call1"
+	//var httpUrl = "http://localhost:8300/team/call1"
+	var httpUrl = "http://172.16.2.133:8300/team/call1"
 	map1 := make(map[string]string)
 	map1["teamName"] = "haha"
 	//result = common.HttpDoi(map1, httpUrl)
